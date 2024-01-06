cba5328524e2e575ccb91ca3576e2a52a6640984
MariiaSam <kontragent@ukr.net>
2024-01-06 19:33:17 +0200

    fix

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,7 +1,9 @@
 import { useDispatch } from "react-redux";
+import { removeTodo } from "store/todoSlice";
+import { toggleTodoCompleted } from "store/todoSlice";
 
 
-const TodoItem = ({ id, text, completed, removeTodo, toggleTodoCompleted }) => {
+const TodoItem = ({ id, text, completed }) => {
   const dispatch = useDispatch()
 
   return (
@@ -9,10 +11,10 @@ const TodoItem = ({ id, text, completed, removeTodo, toggleTodoCompleted }) => {
       <input
         type="checkbox"
         checked={completed}
-        onChange={() => toggleTodoCompleted(id)}
+        onChange={() => dispatch(toggleTodoCompleted({id}))}
       />
       <span>{text}</span>
-      <span className="delete" onClick={() => dispatch()}>
+      <span className="delete" onClick={() => dispatch(removeTodo({id}))}>
   &times;
 </span>
     </li>
