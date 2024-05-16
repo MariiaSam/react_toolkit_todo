78304831877a15b0140b321efbfaf9364f94907f
MariiaSam <kontragent@ukr.net>
2024-05-16 16:55:41 +0300

    clear

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,13 +6,7 @@ import { addTodo } from 'store/todoSlice';
 import TodoList from './TodoList';
 import InputField from './InputField';
 
-import FormButton from './Button/FormButton';
-import RadioFilters from './RadioButton/RadioFilters/RadioFilters';
-import RadioColorCard from './RadioButton/RadioColor/RadioColorCard';
 
-// import Calendar from './Calendar/Calendar';
-
-import Calendar from '1/Calendar';
 
 export function App () {
   const [text, setText] = useState(''); 
@@ -24,24 +18,11 @@ export function App () {
   };
 
 
-  
-  // const removeTodo = todoId => {
-  //   // setTodos(todos.filter(todo => todo.id !== todoId)); //ідентифікатор не дорівнює, що ми отримуєм у ф-ції
-  // };
-
   return (
     <div className="App">
       <InputField text={text} handlerInput={setText} handlerSubmit={addTask} />
       <TodoList />
-
-      <FormButton/>
-      <RadioFilters></RadioFilters>
-      <RadioColorCard></RadioColorCard>
-      {/* <Calendar></Calendar> */}
-      <Calendar></Calendar>
    
-
-
     </div>
   );
 }
--- src/components/Button/FormButton.jsx
+++
@@ -1,7 +0,0 @@
-import css from './FormButton.module.css';
-
-const FormButton = ({ children, type="submit" }) => {
-    return <button type={type} className={css.buttonForm}>Register Now</button>
-}
-
-export default FormButton;
